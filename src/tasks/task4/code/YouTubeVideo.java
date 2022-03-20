19b9a14682d46f35593c6c41a0b6060523295877
Prajjwal <prajjwal199275@gmail.com>
2022-03-20 22:59:07 +0530

    Implemented task4 YouTubeVideo.java class fields and methods

--- src/tasks/task4/code/YouTubeVideo.java
+++ src/tasks/task4/code/YouTubeVideo.java
@@ -1,4 +1,63 @@
 package tasks.task4.code;
 
+import java.math.BigInteger;
+import java.util.Objects;
+
 public class YouTubeVideo {
+    private final long videoId;
+    private final String videoName;
+    private BigInteger numberOfLikes;
+    private int durationInMinutes;
+
+    public YouTubeVideo(long videoId, String videoName) {
+        this.videoId = videoId;
+        this.videoName = videoName;
+    }
+
+    public long getVideoId() {
+        return videoId;
+    }
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public BigInteger getNumberOfLikes() {
+        return numberOfLikes;
+    }
+
+    public void setNumberOfLikes(BigInteger numberOfLikes) {
+        this.numberOfLikes = numberOfLikes;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        YouTubeVideo that = (YouTubeVideo) o;
+        return videoId == that.videoId && durationInMinutes == that.durationInMinutes && Objects.equals(videoName, that.videoName) && Objects.equals(numberOfLikes, that.numberOfLikes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(videoId, videoName, numberOfLikes, durationInMinutes);
+    }
+
+    @Override
+    public String toString() {
+        return "YouTubeVideo{" +
+                "videoId=" + videoId +
+                ", videoName='" + videoName + '\'' +
+                ", numberOfLikes=" + numberOfLikes +
+                ", durationInMinutes=" + durationInMinutes +
+                '}';
+    }
 }

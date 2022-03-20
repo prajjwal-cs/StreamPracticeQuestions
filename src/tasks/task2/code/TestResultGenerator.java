e373b834598ebac915dc7aa47c2cb6da4f697158
Prajjwal <prajjwal199275@gmail.com>
2022-03-20 22:10:03 +0530

    Implemented Main class for task2

--- src/tasks/task2/code/TestResultGenerator.java
+++ src/tasks/task2/code/TestResultGenerator.java
@@ -1,14 +1,25 @@
 package tasks.task2.code;
 
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TestResultGenerator {
+
+    private String studentDetails(Student student) {
+        return student.getStudentName().concat("-").concat(String.valueOf(student.getGuardianContact()));
+    }
+
     public HashSet<String> getDetailsOfFailedStudents(Set<Student> studentSet) {
-        return new HashSet<>();
+        return studentSet.stream().filter(student -> student.getMarks() < 33.0)
+                .map(this::studentDetails).collect(Collectors.toCollection(HashSet<String>::new));
     }
 
     public Set<Student> sortStudentsForRanking(HashSet<Student> studentSet) {
-        return Set.of();
+        Comparator<Student> markSorting = Comparator.comparingDouble(Student::getMarks);
+
+        return studentSet.stream().sorted(markSorting).collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }

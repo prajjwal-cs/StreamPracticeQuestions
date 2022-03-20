77d9c84eb9542e2446a1fe63c54dff8f70039777
Prajjwal <prajjwal199275@gmail.com>
2022-03-20 22:54:04 +0530

    Implemented EmployeeLeaveCalculator.java methods successfully

--- src/tasks/task3/code/EmployeeLeaveCalculator.java
+++ src/tasks/task3/code/EmployeeLeaveCalculator.java
@@ -2,14 +2,21 @@ package tasks.task3.code;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeLeaveCalculator {
     public List<Short> getDefaulterEmployeeIDList(List<Employee> employeeList) {
-        return List.of();
+        return employeeList.stream().filter(employee -> employee.getNumberOfLeaves() > 25)
+                .map(Employee::getEmployeeId).toList();
+    }
+
+    private String employeeDetail(Employee employee) {
+        return employee.getNameOfEmployee().concat("-").concat(String.valueOf(employee.getNumberOfLeaves()));
     }
 
     public ArrayList<String> getNumberOfLeaves(List<Employee> employeeList) {
-        return new ArrayList<>();
+        return employeeList.stream().map(this::employeeDetail)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
 }

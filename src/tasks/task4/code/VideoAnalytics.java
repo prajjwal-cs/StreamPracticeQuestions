fa743fd6500dbf7ca746013eef24de0dc0d08604
Prajjwal <prajjwal199275@gmail.com>
2022-03-21 19:15:57 +0530

    Implemented task4 VideoAnalytics.java class complete

--- src/tasks/task4/code/VideoAnalytics.java
+++ src/tasks/task4/code/VideoAnalytics.java
@@ -1,15 +1,26 @@
 package tasks.task4.code;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class VideoAnalytics {
     public HashSet<YouTubeVideo> getTopTenViralVideos(List<YouTubeVideo> videos) {
-        return new HashSet<>();
+        return videos.stream().sorted(Comparator.comparing(YouTubeVideo::getNumberOfLikes)
+                        .thenComparing(YouTubeVideo::getDurationInMinutes)).limit(10)
+                .collect(Collectors.toCollection(HashSet<YouTubeVideo>::new));
+    }
+
+    private String detailsOfVideo(YouTubeVideo youTubeVideo) {
+        return youTubeVideo.getVideoName().concat(" - ")
+                .concat(String.valueOf(youTubeVideo.getNumberOfLikes()))
+                .concat(String.valueOf(youTubeVideo.getDurationInMinutes()));
     }
 
     public List<String> showDetailsOfShortVideos(Set<YouTubeVideo> videos) {
-        return List.of();
+        return videos.stream().filter(e -> e.getDurationInMinutes() <= 10)
+                .map(this::detailsOfVideo).toList();
     }
 }

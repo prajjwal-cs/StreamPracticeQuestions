1be0a31abf719075716b6c6634bf470b5bfdfd31
Prajjwal <prajjwal199275@gmail.com>
2022-03-25 14:51:25 +0530

    Implemented one method in TeacherHelper.java class

--- src/tasks/task5/code/TeacherHelper.java
+++ src/tasks/task5/code/TeacherHelper.java
@@ -5,7 +5,9 @@ import java.util.Set;
 
 public class TeacherHelper {
     public List<Teacher> findSubjectTeacher(List<Teacher> allTeachers, String subjectName) {
-        return List.of();
+        return allTeachers.stream()
+                .filter(e -> e.getTeacherSubject().contains(subjectName))
+                .toList();
     }
 
     public Set<Teacher> getBusyTeachers(List<Teacher> allTeachers) {

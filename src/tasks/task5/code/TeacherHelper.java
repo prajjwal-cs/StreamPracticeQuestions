d0c14b0228cf7fd394961b0d576232bbffd0e65c
Prajjwal <prajjwal199275@gmail.com>
2022-03-25 23:02:43 +0530

    Implemented another method in TeacherHelper.java class

--- src/tasks/task5/code/TeacherHelper.java
+++ src/tasks/task5/code/TeacherHelper.java
@@ -2,6 +2,8 @@ package tasks.task5.code;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class TeacherHelper {
     public List<Teacher> findSubjectTeacher(List<Teacher> allTeachers, String subjectName) {
@@ -10,7 +12,11 @@ public class TeacherHelper {
                 .toList();
     }
 
+
     public Set<Teacher> getBusyTeachers(List<Teacher> allTeachers) {
-        return Set.of();
+        Predicate<Teacher> predicate = teacher -> teacher.getTeacherSubject().size() >= 3;
+        return allTeachers.stream().filter(predicate)
+                .collect(Collectors.toSet());
     }
+
 }

b59794e932b5f94f739fb097766776d289c56143
Prajjwal <prajjwal199275@gmail.com>
2022-03-27 23:23:12 +0530

    Implemented in Task7 CollegeLibrary.java class

--- src/tasks/task7/code/CollegeLibrary.java
+++ src/tasks/task7/code/CollegeLibrary.java
@@ -2,13 +2,15 @@ package tasks.task7.code;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CollegeLibrary {
     public List<Book> getListOfExpensiveBooks(Set<Book> books) {
-        return List.of();
+        return books.stream().filter(book -> book.getBookPrice() >= 5000.0).distinct()
+                .toList();
     }
 
     public Book findBookDetailsByISBN(List<Book> books, String ISBN) {
-        return new Book();
+        return (Book) books.stream().filter(book -> book.getIsbnNumber().equals(ISBN));
     }
 }

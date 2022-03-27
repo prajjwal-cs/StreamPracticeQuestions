8db455005cc2b7d430f0d536be6513b4c4d5362a
Prajjwal <prajjwal199275@gmail.com>
2022-03-27 23:02:13 +0530

    Implemented in Task6 OnlineShop.java class

--- src/tasks/task6/code/OnlineShop.java
+++ src/tasks/task6/code/OnlineShop.java
@@ -6,14 +6,14 @@ import java.util.Set;
 
 public class OnlineShop {
     public List<SmartPhone> getListOfSmartPhones(Set<SmartPhone> smartPhones, byte requiredScreenSize) {
-        smartPhones.stream()
-                .filter(smartPhone -> smartPhone.getScreenSize() >= requiredScreenSize);
         smartPhones.stream().filter(smartPhone -> smartPhone.getScreenSize() < requiredScreenSize)
                 .peek(e -> e.setSmartPhonePrice(e.getSmartPhonePrice() - e.getSmartPhonePrice() * 0.1));
-        return smartPhones.stream().toList();
+        return smartPhones.stream()
+                .filter(smartPhone -> smartPhone.getScreenSize() >= requiredScreenSize)
+                .toList();
     }
 
     public List<SmartPhone> findBudgetSmartPhones(Set<SmartPhone> smartPhones) {
-        return List.of();
+        return smartPhones.stream().filter(smartPhone -> smartPhone.getSmartPhonePrice() <= 10000.0).toList();
     }
 }
